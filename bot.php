6d65ce64e6ef69f31a91896cfa7e163cc374c2be
poocheez <pooparazza@hotmail.com>
2017-03-11 21:32:59 +0700

    -

--- bot.php
+++ bot.php
@@ -1,8 +1,21 @@
 ﻿<?php
-include 'vendor/autolod.php';
+require __DIR__."/../vendor/autoload.php";
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('SMYNoJUaCeRSLcQNK2pYwx1IvTrHhxh4mIQL/tweCR8/4hMuiB72A7XDp6mMDWLQoCL45e1uOmdy4zrUza0B1i20us0ITw7B/+CvixgEX6p6Kzgne9C0NlqbFqDGw71dgB6ywQNX/PQiUpVUPYXXNgdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'https://api.line.me/v2/bot/message/reply']);
-$response = $bot->replyText('<reply token>', 'hello!');
+define("LINE_MESSAGING_API_CHANNEL_SECRET", 'b41689685c7ba1f9c06cf19f91153138');
+define("LINE_MESSAGING_API_CHANNEL_TOKEN", 'SMYNoJUaCeRSLcQNK2pYwx1IvTrHhxh4mIQL/tweCR8/4hMuiB72A7XDp6mMDWLQoCL45e1uOmdy4zrUza0B1i20us0ITw7B/+CvixgEX6p6Kzgne9C0NlqbFqDGw71dgB6ywQNX/PQiUpVUPYXXNgdB04t89/1O/w1cDnyilFU=');
 
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]);
+$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$body = file_get_contents("php://input");
+
+$events = $bot->parseEventRequest($body, $signature);
+
+foreach ($events as $event) {
+    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+        $reply_token = $event->getReplyToken();
+        $text = $event->getText();
+        $bot->replyText($reply_token, $text);
+    }
+}
 ?>
