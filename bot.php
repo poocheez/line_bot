05a23a981ea25dd332111e5aaa66397a98eccacc
poocheez <pooparazza@hotmail.com>
2017-03-11 20:30:48 +0700

    -

---
+++ bot.php
@@ -0,0 +1,9 @@
+﻿<?php
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('SMYNoJUaCeRSLcQNK2pYwx1IvTrHhxh4mIQL/tweCR8/4hMuiB72A7XDp6mMDWLQoCL45e1uOmdy4zrUza0B1i20us0ITw7B/+CvixgEX6p6Kzgne9C0NlqbFqDGw71dgB6ywQNX/PQiUpVUPYXXNgdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1505189591']);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+?>
